9bf148124d7e89f61647fcc0445dee7111ce3193
Camilo Estrada Patiño <caespa0818@hotmail.es>
2024-09-11 14:40:55 -0500

    Metapixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter , Poppins } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 const poppins = Poppins({
@@ -23,6 +24,25 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="shortcut icon" href='/Images/Logo.svg' />
+
+        <Script id="meta-pixel-code" strategy="afterInteractive">
+          {`
+          !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${process.env.NEXT_PUBLIC_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+          <noscript><img height="1" width="1" style={{ display: 'none' }} alt="meta"
+            src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_PIXEL_ID}&ev=PageView&noscript=1`}
+            /></noscript>
+
+        </Script>
       </head>
       <body className={`bg-black relative  ${poppins.className}`}>{children}</body>
     </html>
